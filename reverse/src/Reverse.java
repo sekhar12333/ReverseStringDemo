feaab83d415dd2752910637d2cbb247799b801b3
Sekhar Pasem <spasem@cognitivescale.com>
2018-11-22 22:37:04 +0530

    seccond message

--- reverse/src/Reverse.java
+++ reverse/src/Reverse.java
@@ -1,5 +1,8 @@
 public class Reverse {
     public static void main(String[] args) {
+        System.out.println("hI");
+        System.out.println("hello");
         System.out.println("Hello world...");
+        System.out.println("Hhi");
     }
 }
